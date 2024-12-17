ecf02e3e9aa02d60e2b0258866c67dcb550410bd
Daria <82290545+dariadragomir@users.noreply.github.com>
2024-12-17 17:15:54 +0200

    Create stable_diffusion_mps_refiner_pipeline.py

---
+++ Photo generation - genAI/Intermediate steps/stable_diffusion_mps_refiner_pipeline.py
@@ -0,0 +1,40 @@
+from diffusers import DiffusionPipeline
+import torch
+
+base = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-base-1.0", 
+    torch_dtype=torch.float16, 
+    variant="fp16", 
+    use_safetensors=True
+)
+base.to("mps")
+
+refiner = DiffusionPipeline.from_pretrained(
+    "stabilityai/stable-diffusion-xl-refiner-1.0",
+    text_encoder_2=base.text_encoder_2,
+    vae=base.vae,
+    torch_dtype=torch.float16,
+    use_safetensors=True,
+    variant="fp16",
+)
+refiner.to("mps")
+
+n_steps = 20
+high_noise_frac = 0.8
+
+prompt = "realistic photo of a pretty boy"
+
+image = base(
+    prompt=prompt,
+    num_inference_steps=n_steps,
+    denoising_end=high_noise_frac,
+    output_type="latent",
+).images
+image = refiner(
+    prompt=prompt,
+    num_inference_steps=n_steps,
+    denoising_start=high_noise_frac,
+    image=image,
+).images[0]
+
+image.save("final_image.png")
